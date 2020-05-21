8a887e5445fa75ee5b8a262b49b4b638f33d1f7d
Gabriel Zellmer <1470179+CrossEyeORG@users.noreply.github.com>
2020-05-21 11:23:25 -0700

    Fixing source to be POSIX compliant

--- bin/hass_updater.sh
+++ bin/hass_updater.sh
@@ -1,25 +1,30 @@
 #!/bin/bash
-# CrossEye Homeassistant Update Script
+# CrossEye Homeassistant Virtual Environment Update Script
 
+# Global variables
 HASS_INSTALLED_VERSION=$(/srv/homeassistant/bin/hass --version)
-HASS_AVAILABLE_VERSION=$(curl --silent "https://api.github.com/repos/home-assistant/core/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")')
+HASS_AVAILABLE_VERSION=$(/usr/bin/curl --silent "https://api.github.com/repos/home-assistant/core/releases/latest" | /usr/bin/grep -Po '"tag_name": "\K.*?(?=")')
 HASS_VIRTUALENV_USER="homeassistant"
+HASS_SERVICE_NAME="homeassistant.service"
+
+# Emitting logs to Syslog
+exec 1> >(logger -s -t $(basename $0)) 2>&1
 
 cat << EOF
 [INFO] Version check...
-Currently version installed: $HASS_INSTALLED_VERSION
+Current version installed: $HASS_INSTALLED_VERSION
 Available version in repos: $HASS_AVAILABLE_VERSION
 EOF
 
 if (( $(awk 'BEGIN {print ("'$HASS_AVAILABLE_VERSION'" > "'$HASS_INSTALLED_VERSION'")}') )); then
     printf "[INFO] Newer version available, beginning upgrade...\n"
-    sudo service homeassistant stop
+    /usr/bin/systemctl stop $HASS_SERVICE_NAME
     sudo -u $HASS_VIRTUALENV_USER -H -s << 'EOF'
-        source /srv/homeassistant/bin/activate
-        pip3 install --upgrade homeassistant
+        . /srv/homeassistant/bin/activate
+        /usr/bin/pip3 install --upgrade homeassistant
         exit
 EOF
-    sudo service homeassistant start
+    /usr/bin/systemctl start $HASS_SERVICE_NAME
 else
     printf "[INFO] Latest version installed, skipping upgrade...\n"
 fi
