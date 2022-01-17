76a8e33b6247d9f25bd458a4eabdd2a0b5d08b90
Gabriel Zellmer <1470179+CrossEyeORG@users.noreply.github.com>
2022-01-17 12:09:34 -0800

    Updating logic to handle date versions

--- hass_updater.sh
+++ hass_updater.sh
@@ -4,7 +4,7 @@
 #Description    : Auto updates Home Assistant (HASS) Python virtual environment.
 #Author         : Gabriel Zellmer (CrossEye)
 #Repository     : https://github.com/CrossEyeORG/HASS
-#Version        : 1.0
+#Version        : 1.1
 #Notes          : Needs the globals configured to your local environment and added to a cron job.
 ################################################################
 
@@ -22,10 +22,13 @@ exec 1> >(logger -s -t $(basename $0)) 2>&1
 cat << EOF
 [INFO] Version check...
 Current version installed: $HASS_INSTALLED_VERSION
-Available version in repos: $HASS_AVAILABLE_VERSION
+Available version in GitHub: $HASS_AVAILABLE_VERSION
 EOF
 
-if (( $(awk 'BEGIN {print ("'$HASS_AVAILABLE_VERSION'" > "'$HASS_INSTALLED_VERSION'")}') )); then
+# Convert date versions to comparable strings
+function version { echo "$@" | awk -F. '{ printf("%d%03d%03d%03d\n", $1,$2,$3,$4); }'; }
+
+if [ $(version ${HASS_INSTALLED_VERSION}) -lt $(version ${HASS_AVAILABLE_VERSION}) ]; then
     printf "[INFO] Newer version available, beginning upgrade...\n"
     /usr/bin/systemctl stop $HASS_SERVICE_NAME
     sudo -u $HASS_VIRTUALENV_USER -H -s << EOF
@@ -35,8 +38,11 @@ if (( $(awk 'BEGIN {print ("'$HASS_AVAILABLE_VERSION'" > "'$HASS_INSTALLED_VERSI
         exit
 EOF
     /usr/bin/systemctl start $HASS_SERVICE_NAME
+    exit 1
+elif [ $(version ${HASS_INSTALLED_VERSION}) -gt $(version ${HASS_AVAILABLE_VERSION}) ]; then
+    printf "[INFO] Running a version newer than what's available in GitHub, skipping upgrade...\n"
+    exit 0
 else
     printf "[INFO] Latest version installed, skipping upgrade...\n"
+    exit 0
 fi
-
-exit
